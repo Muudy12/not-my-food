141f0e798b1239a6fd0a908d42df7856b782f7e6
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-08-28 10:06:12 -0700

    routes started to add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <h1>Not My Food</h1>
+    <h1>Not My Food</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
