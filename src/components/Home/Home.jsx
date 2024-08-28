540daccfb59bc96a4df1a75e9c07ad2a94a37535
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-08-28 13:28:24 -0700

    broken by sophia

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import xIcon from '../../assets/x.svg'
 import checkIcon from '../../assets/check.svg'
 import './Home.scss'
 
-function Home({foods}) {
+function Home({foods, setLikes, setDisLikes}) {
 
   const min = 1;
   const max = 20;
   let randomNum = Math.floor(Math.random()*(max - min + 1));
+  const [currentImg, setCurrentImg] = useState(null);
 
-  const [currentImg, setCurrentImg] = useState(foods.find((food) => food.id === randomNum ));
+  useEffect(() => {
+    const current = foods.find(f => f.id === `${randomNum}`);
+    setCurrentImg(current);
+  }, [foods,randomNum])
 
 
+
+  const handleLikeChange = () => {
+    setLikes(prevLikes => [...prevLikes, currentImg]);
+    setCurrentImg(null);
+  }
+
+  const handleDisLikeChange = () => {
+    setDisLikes(prevDisLikes => [...prevDisLikes, currentImg]);
+    setCurrentImg(null);
+  }
 
   return (
     <main className='main'>
       <div className='main__container-image'>
-        <img className='main__image' alt='photo of food to like or dislike' src={foods}img/>
+        <img className='main__image' alt='photo of food to like or dislike' src={currentImg}/>
       </div>
 
       <div className='main__container'>
-        <button className='main__btn'>
+        <button className='main__btn' on >
           <img className='main__btn-img' src={xIcon} alt='image of x' />
         </button>
 
