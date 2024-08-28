b1ee38f35be482292c75a16dd23e4bf24bdb1049
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-08-28 10:20:15 -0700

    Routes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Preferences from './components/Preferences/Preferences';
+import FoodDetails from './components/FoodDetails/FoodDetails';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
 
@@ -7,10 +13,14 @@ function App() {
     <>
     <h1>Not My Food</h1>
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route />
-
+          <Route path='/' element={<Home/>} />
+          <Route path='/fooder' element={<Preferences/>} />
+          <Route path='/fooder/:id' element={<FoodDetails/>} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
 
     </>
