65ba4f8938702555a433d5f3ebec67b5a3ec651b
Mu Dah <dahmu24@gmail.com>
2024-09-20 18:54:17 -0600

    Added link back to Mu-port

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,33 @@
-import './Header.scss';
+import "./Header.scss";
 import { Link } from "react-router-dom";
 import avaterIcon from "../../assets/avater.png";
-import logo2 from '../../assets/not-my-food-high-resolution-logo-transparent.png';
+import logo2 from "../../assets/not-my-food-high-resolution-logo-transparent.png";
+import MuPort from "../../assets/mu-port.png";
 
 function Header() {
-    return (
-        <header className="header">
-            <nav className="header__nav">
-                <Link to="/" className="header__link header__logo-link">
-                    <img src={logo2} className="header__logo" alt="Not My Food Logo" />
-                </Link>
-                <div className='header__container'>
-                <Link to="/" className="header__link header__home-link">
-                    <h3>Choices</h3>
-                </Link>
-                <Link to="/fooder" className="header__link header__home-link">
-                    <h3>Preferences</h3>
-                </Link>
-                <Link to="/fooder" className="header__link header__profile-link">
-                    <img className="header__avatar" src={avaterIcon} alt="Profile" />
-                </Link>
-                </div>
-
-
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <Link className="mu-port" to="https://muport.netlify.app/">
+        <img src={MuPort} alt="Mu Port Logo" />
+      </Link>
+      <nav className="header__nav">
+        <Link to="/" className="header__link header__logo-link">
+          <img src={logo2} className="header__logo" alt="Not My Food Logo" />
+        </Link>
+        <div className="header__container">
+          <Link to="/" className="header__link header__home-link">
+            <h3>Choices</h3>
+          </Link>
+          <Link to="/fooder" className="header__link header__home-link">
+            <h3>Preferences</h3>
+          </Link>
+          <Link to="/fooder" className="header__link header__profile-link">
+            <img className="header__avatar" src={avaterIcon} alt="Profile" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
